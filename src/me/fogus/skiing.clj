dd226e96789558c3e6285bec9c8974ceac093881
fogus <mefogus@gmail.com>
2010-09-01 15:10:28 -0400

    added tests for Z and played with cleave

--- src/me/fogus/skiing.clj
+++ src/me/fogus/skiing.clj
@@ -1,45 +1,85 @@
 (ns me.fogus.skiing)
 
-;; Sxyz → xz(yz)
-(defn S [x] 
-  (fn [y] 
-    (fn [z] 
-      ((x z) (y z)))))
+;; Sfgx → fx(gx)
+(defn S [f] 
+  (fn [g] 
+    (fn [x] 
+      ((f x) (g x)))))
+
+(def starling S)
 
 ;; Kxy → x
 (defn K [x]
   (fn [y]
     x))
 
+(def kestrel K)
+
+;; Ix → x
 (defn I [x] 
   (((S K) K) x))
 
-(defn B [x]
-  (fn [y]
-    (fn [z]
-      (x (y z)))))
+(def idiot I)
 
-(defn C [x]
-  (fn [y]
-    (fn [z]
-      ((x z) y))))
+;; Bfgx → f(gx)
+(defn B
+  "The composition of f and g"
+  [f]
+  (fn [g]
+    (fn [x]
+      (f (g x)))))
 
-(defn W [x]
-  (fn [y]
-    ((x y) y)))
+(def bluebird B)
+
+;; Cfxy → fyx
+(defn C
+  "Swaps the args x and y"
+  [f]
+  (fn [x]
+    (fn [y]
+      ((f y) x))))
+
+(def cardinal C)
+
+;; Wfx → fxx
+(defn W
+  "Dups the arg b"
+  [f]
+  (fn [x]
+    ((f x) x)))
+
+(def warbler W)
 
 (defn Y [r]
   ((fn [f] (f f))
    (fn [f]
      (r 
-      (fn [x] 
-        ((f f) x))))))
+      (fn [a] 
+        ((f f) a))))))
 
-(defn M [x] 
-  (((S I) I) x))
+(def sage Y)
+
+
+(defn Z [f]
+  ((fn [x] (f (fn [y] ((x x) y))))
+   (fn [x] (f (fn [y] ((x x) y))))))
+
+(def sage-once-removed Z)
+
+
+(defn M [a] 
+  (((S I) I) a))
+
+(def mockingbird M)
+
  
 (defn U [f]
   (f f))
+
+(def turing U)
+
+
+;; currying/uncurrying
 
 (defn curry2 [f]
   (fn [a]
@@ -51,3 +91,18 @@
     (if args
       (uncurry (apply partial f args))
       (f))))
+
+;; cleave
+
+(defn cleave
+  [& args]
+  ((apply juxt (butlast args))
+   (last args)))
+
+
+(cleave 42 int str)
+((juxt int str) 42)
+
+(defn cleave2
+  [x & fns]
+  ((apply juxt fns) x))
