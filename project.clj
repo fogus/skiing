c5e57547f46f6d42644bf5ea42b5796e04b6eb08
fogus <mefogus@gmail.com>
2011-12-11 16:53:58 -0500

    Some more docs and aliasing

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
 (defproject skiing "0.0.1"
   :description "a combinator zoo"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dev-dependencies [[jline "0.9.94"]
+                     [swank-clojure "1.3.3"]
+                     [marginalia "0.6.1"]
+                     [lein-marginalia "0.6.0"]])
--- src/me/fogus/skiing.clj
+++ src/me/fogus/skiing.clj
@@ -1,54 +1,67 @@
 (ns me.fogus.skiing)
 
 ;; Sfgx → fx(gx)
-(defn S [f] 
+(defn S
+  ""
+  [f] 
   (fn [g] 
     (fn [x] 
       ((f x) (g x)))))
 
 (def starling S)
+(def <*> S)
 
 ;; Kxy → x
-(defn K [x]
+(defn K
+  "The constant combinator.  Similar to Clojure's `constantly` and Haskell's `pure`."
+  [x]
   (fn [y]
     x)) 
 
 (def kestrel K)
+(def pure K)
 
 ;; Ix → x
-(defn I [x] 
+(defn I
+  "The identity combinator.  Similar to Clojure's `identity` and Haskell's `id`."
+  [x] 
   (((S K) K) x))
 
 (def idiot I)
+(def id I)
 
 ;; Bfgx → f(gx)
 (defn B
-  "The composition of f and g"
+  "The composition of f and g.  This is akin to the `<$>` operator in Haskell."
   [f]
   (fn [g]
     (fn [x]
       (f (g x)))))
 
 (def bluebird B)
+(def <$> B)
 
 ;; Cfxy → fyx
 (defn C
-  "Swaps the args x and y"
+  "Swaps the args x and y.  This is the same as the `flip` function in Haskell."
   [f]
   (fn [x]
     (fn [y]
       ((f y) x))))
 
 (def cardinal C)
+(def flip C)
 
 ;; Wfx → fxx
 (defn W
-  "Dups the arg b"
+  "Dups the arg b.  This is similar to Haskell's `join`."
   [f]
   (fn [x]
     ((f x) x)))
 
 (def warbler W)
+(def join W)
+
 
 (defn Y [r]
   ((fn [f] (f f))
