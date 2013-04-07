470202717087ef51432e849d25fe9cae14a6a356
Fogus <mefogus@gmail.com>
2013-04-07 09:50:26 -0300

    Upgrade Clojure version to v1.5.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject skiing "0.0.1"
   :description "a combinator zoo"
-  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :dev-dependencies [[jline "0.9.94"]
                      [swank-clojure "1.3.3"]
                      [marginalia "0.6.1"]
